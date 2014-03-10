fb2806bfd3949ba448ba3f9c9f3de2bde2b23cb4
Julien Barnier <julien@nozav.org>
2014-03-10 13:00:41 +0100

    Réorganisation et ajout 5 dernières journées

--- src/simulations.R
+++ src/simulations.R
@@ -9,11 +9,9 @@ load("cache/d.RData")
 
 nb.rep <- 1000
 
-
 ## Méthode 1 : probas sur toutes les journées précedentes
 
 probas <- table.probas(d)
-
 resultats <- mclapply(1:nb.rep, simulation, probas)
 
 table.all <- rbindlist(resultats)
@@ -23,6 +21,16 @@ cache("table.all")
 ## Méthode 2 : probas sur les 15 dernières journées
 
 probas <- table.probas(d, derniers=15)
+
+resultats <- mclapply(1:nb.rep, simulation, probas)
+
+table15 <- rbindlist(resultats)
+cache("table15")
+
+
+## Méthode 3 : probas sur les 5 dernières journées
+
+probas <- table.probas(d, derniers=5)
 
 resultats <- mclapply(1:nb.rep, simulation, probas)
 
