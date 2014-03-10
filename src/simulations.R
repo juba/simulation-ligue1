e54a6b69007b985b9931e637995693cc8a6e1623
Julien Barnier <julien@nozav.org>
2014-03-10 13:02:43 +0100

    Correction typos

--- src/simulations.R
+++ src/simulations.R
@@ -12,29 +12,27 @@ nb.rep <- 1000
 ## Méthode 1 : probas sur toutes les journées précedentes
 
 probas <- table.probas(d)
-resultats <- mclapply(1:nb.rep, simulation, probas)
-
+resultats <- mclapply(1:nb.rep, simulation, d, probas)
 table.all <- rbindlist(resultats)
+
 cache("table.all")
 
 
 ## Méthode 2 : probas sur les 15 dernières journées
 
 probas <- table.probas(d, derniers=15)
-
-resultats <- mclapply(1:nb.rep, simulation, probas)
-
+resultats <- mclapply(1:nb.rep, simulation, d, probas)
 table15 <- rbindlist(resultats)
+
 cache("table15")
 
 
 ## Méthode 3 : probas sur les 5 dernières journées
 
 probas <- table.probas(d, derniers=5)
+resultats <- mclapply(1:nb.rep, simulation, d, probas)
+table5 <- rbindlist(resultats)
 
-resultats <- mclapply(1:nb.rep, simulation, probas)
-
-table15 <- rbindlist(resultats)
-cache("table15")
+cache("table5")
 
 
