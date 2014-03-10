f800468f6e47de1ab8f2395ef8a7175d9920e444
Julien Barnier <julien@nozav.org>
2014-03-10 12:53:45 +0100

    Fusion scrape et recode

--- munge/01-scrape.R
+++ munge/01-scrape.R
@@ -16,5 +16,24 @@ for (i in 1:length(tables)) {
 
 d <- rbindlist(tables)
 
+d <- within(d, {
+    ## Matchs à venir
+    score[score=="-"] <- NA
+    score[!grepl("-", score)] <- NA
+    ## Extraction score
+    buts.dom <- gsub("-[0-9]+$","",score)
+    buts.ext <- gsub("^[0-9]+-","",score)
+    ## Recodage résultat
+    result <- NA
+    result[buts.dom > buts.ext] <- "dom"
+    result[buts.dom < buts.ext] <- "ext"
+    result[buts.dom == buts.ext] <- "nul"
+    ## Recodage équipes
+    dom <- as.character(eq.dom)
+    ext <- as.character(eq.ext)
+})
+
+d <- d[,list(dom,ext,journee,result)]
+
 cache("d")
 
