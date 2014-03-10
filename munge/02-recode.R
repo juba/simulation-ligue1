a0753c1478e086bb72a06aca4463abf55b6c12b0
Julien Barnier <julien@nozav.org>
2014-03-10 12:52:31 +0100

    Renommage de modalités

--- munge/02-recode.R
+++ munge/02-recode.R
@@ -13,9 +13,14 @@ d <- within(d, {
     buts.ext <- gsub("^[0-9]+-","",score)
     ## Recodage résultat
     result <- NA
-    result[buts.dom > buts.ext] <- "eq.dom"
-    result[buts.dom < buts.ext] <- "eq.ext"
+    result[buts.dom > buts.ext] <- "dom"
+    result[buts.dom < buts.ext] <- "ext"
     result[buts.dom == buts.ext] <- "nul"
+    ## Recodage équipes
+    dom <- as.character(eq.dom)
+    ext <- as.character(eq.ext)
 })
+
+d <- d[,list(dom,ext,journee,result)]
 
 cache("d")
