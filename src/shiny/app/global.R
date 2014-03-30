896d5bf3d11bf685e03039409e0c3dfb7754d4e4
Julien Barnier <julien@nozav.org>
2014-03-30 23:26:07 +0200

    Mise à jour Ligue 1 et National

--- src/shiny/app/global.R
+++ src/shiny/app/global.R
@@ -5,9 +5,9 @@ datas <- data.frame(championnat=character(),
                     journee.min=numeric(),
                     journee.max=numeric(),
                     stringsAsFactors=FALSE)
-datas[1,] <- c("Ligue 1",  "2013-2014", 20, 30)
+datas[1,] <- c("Ligue 1",  "2013-2014", 20, 31)
 datas[2,] <- c("Ligue 2",  "2013-2014", 20, 28)
-datas[3,] <- c("National", "2013-2014", 22, 25)
+datas[3,] <- c("National", "2013-2014", 22, 26)
 
 #load("www/data/datas.Rdata") # datas
 
