f0d8a20681c506dccce5409308d79b8c60b566be
Julien Barnier <julien@nozav.org>
2014-04-08 09:46:42 +0200

    Mise à jour Ligue2

--- src/shiny/app/global.R
+++ src/shiny/app/global.R
@@ -6,7 +6,7 @@ datas <- data.frame(championnat=character(),
                     journee.max=numeric(),
                     stringsAsFactors=FALSE)
 datas[1,] <- c("Ligue 1",  "2013-2014", 20, 32)
-datas[2,] <- c("Ligue 2",  "2013-2014", 20, 30)
+datas[2,] <- c("Ligue 2",  "2013-2014", 20, 31)
 datas[3,] <- c("National", "2013-2014", 22, 26)
 
 #load("www/data/datas.Rdata") # datas
