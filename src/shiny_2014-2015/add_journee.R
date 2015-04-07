83c48d2ac6ca0e1e9c6521b2f60247e77b7b4849
Julien Barnier <julien@nozav.org>
2015-04-07 11:19:23 +0200

    Mise à jour

--- src/shiny_2014-2015/add_journee.R
+++ src/shiny_2014-2015/add_journee.R
@@ -26,11 +26,11 @@ add.journee <- function(championnat, saison, journee, nb.rep=5000) {
 
 ## Ajout d'une nouvelle journée
 
-add.journee("Ligue 1", "2014-2015", 29, 5000)
+add.journee("Ligue 1", "2014-2015", 31, 5000)
 
-add.journee("Ligue 2", "2014-2015", 28, 5000)
+add.journee("Ligue 2", "2014-2015", 30, 5000)
 
-add.journee("National", "2014-2015", 24, 5000)
+add.journee("National", "2014-2015", 27, 5000)
 
 
 # for (i in 21:26) {
