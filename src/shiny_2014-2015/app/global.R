2902e799d48958bf831b663bcdfe6570b5d812b0
Julien Barnier <julien@nozav.org>
2015-05-18 22:42:24 +0200

    Mise à jour

--- src/shiny_2014-2015/app/global.R
+++ src/shiny_2014-2015/app/global.R
@@ -6,9 +6,9 @@ datas <- data.frame(championnat=character(),
                     journee.min=numeric(),
                     journee.max=numeric(),
                     stringsAsFactors=FALSE)
-datas[1,] <- c("Ligue 1",  "2014-2015", 20, 35)
-datas[2,] <- c("Ligue 2",  "2014-2015", 20, 35)
-datas[3,] <- c("National", "2014-2015", 18, 30)
+datas[1,] <- c("Ligue 1",  "2014-2015", 20, 37)
+datas[2,] <- c("Ligue 2",  "2014-2015", 20, 37)
+datas[3,] <- c("National", "2014-2015", 18, 33)
 
 saison <- "2014-2015"
 
